3c912f8e1587478b18b6be968b2023b17731a046
Elliot B. <elliot.barthelemy@etu.umontpellier.fr>
2026-02-11 09:09:22 +0100

    wenamechainasama, tumajarbitsaun

--- PlatformerEngine/obj/Debug/net8.0/PlatformerEngine.AssemblyInfo.cs
+++ PlatformerEngine/obj/Debug/net8.0/PlatformerEngine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformerEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3577a743a91c6a2f8f7498ab1b6c533c76dbf440")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b57c2011720e73122a0db933fb21360d31f57369")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformerEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformerEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
